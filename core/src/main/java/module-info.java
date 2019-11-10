738ef0793feec799f4e4d5fc7bcfeb4b06116f4b
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-11-10 22:33:19 +0100

    improved

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -2,6 +2,9 @@
  * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
+/**
+ * Provides validation API and infrastructure.
+ */
 module io.github.mmm.validation {
   exports io.github.mmm.validation;
 }
