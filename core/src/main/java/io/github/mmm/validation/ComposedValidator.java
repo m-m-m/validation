d4579460f6fb682255fdcf56f145e5397bc905a3
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-12-04 17:24:25 +0100

    fixed JavaDoc

--- core/src/main/java/io/github/mmm/validation/ComposedValidator.java
+++ core/src/main/java/io/github/mmm/validation/ComposedValidator.java
@@ -4,8 +4,8 @@ package io.github.mmm.validation;
 
 /**
  * {@link Validator} that is composed out of a set of individual {@link #getChild(int) validators} given at
- * {@link #ComposedValidator(AbstractValidator...) construction}. It will always invoke <em>all</em>
- * {@link #getChild(int) contained validators} when a {@link #validate(Object, Object) validation} is performed.
+ * {@link #ComposedValidator(Validator...) construction}. It will always invoke <em>all</em> {@link #getChild(int)
+ * contained validators} when a {@link #validate(Object, Object) validation} is performed.
  *
  * @param <V> is the generic type of the value to {@link #validate(Object) validate}.
  *
